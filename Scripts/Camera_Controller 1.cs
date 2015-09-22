88a4edeca497c5ae1952c5006d4d32b3e04befcc
AdolfDupler <dupesofhazard@gmail.com>
2015-09-22 11:45:01 -0700

    Moves the camera around.
    Noah made this.

---
+++ Scripts/Camera_Controller 1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Controller : MonoBehaviour {
+
+
+	float x;
+	float z;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//x=(Input.mousePosition.x - Screen.width/2)/20;
+		//y=(Input.mousePosition.y - Screen.height/2)/20;
+		x += Input.GetAxis ("Horizontal")/3;
+		z += Input.GetAxis ("Vertical")/3;
+
+		transform.position = new Vector3(x,10,z);
+
+		/*if (Input.GetButton("Fire1")) {
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = new Vector3(x, 0, z);
+            cube.AddComponent<changify>();
+		}*/
+
+	}
+}
